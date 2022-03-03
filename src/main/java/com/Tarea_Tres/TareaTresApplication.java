accae0c252670455499c792d05b0941d43aa0beb
ivanc <ivancototkd@gmail.com>
2022-03-03 08:37:25 -0600

    Primer commit de la tarea tres de la semana seis

---
+++ src/main/java/com/Tarea_Tres/TareaTresApplication.java
@@ -0,0 +1,13 @@
+package com.Tarea_Tres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaTresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaTresApplication.class, args);
+	}
+
+}
